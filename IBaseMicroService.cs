6e144398432dc260d51eef65e7219a300f568be1
Roxana María Garrido Sánchez <garrido.sanchez.roxana.maria@alumnat.copernic.cat>
2023-09-05 14:30:55 +0200

    commit 5_9_23

---
+++ IBaseMicroService.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Base_MicroService
+{
+    public interface IBaseMicroService
+    {
+        void Start(HostControl hc);
+        void Stop();
+        void Pause();
+        void Continue();
+        void Shutdown();
+        void Resume();
+        void TryRequest(Action action, int maxFailures, int startTimeoutMS = 100,
+            int resetTimeout = 10000, Action<Exception> onError = null);
+
+        Task TryRequestAsync([NotNull] Func<Task> action, int maxFailures,
+            int startTimeoutMS = 100, int resetTimeout = 10000, [CanBeNull]
+            Action<Exception> onError = null);
+
+        void PublishMessage(object message, string connStr = "host=localhost", string topic = "");
+
+        Task PublishMessageAsync(object message, string connStr = "host=localhost", string topic = "");
+
+    }
+
+}
